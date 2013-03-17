0043391e126a78b9c43cb71a3d9ab7f7e1b97ac7
Ilya Krukov <IKrukov@gmail.com>
2013-03-17 22:50:30 +0400

    Add mouse-3 global binding to context menu

--- bindings.el
+++ bindings.el
@@ -1,5 +1,7 @@
-;; Change active window - Alt+arrow 
+;; Change active window - Alt+arrow
 (windmove-default-keybindings 'meta)
 (global-set-key "\C-Z" 'undo)
+
+(global-set-key [(mouse-3)] 'mouse-major-mode-menu)
 
 (provide 'bindings )
