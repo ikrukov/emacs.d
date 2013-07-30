54f1b02afe79ba7940f05553fdff277f2768fd41
Ilya Krukov <IKrukov@gmail.com>
2013-07-30 22:49:51 +0400

    Haskell and others

---
+++ haskell-settings.el
@@ -0,0 +1,27 @@
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+
+;; ;Set indent mode - only 
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+;;(add-hook 'haskell-mode-hook 'haskell-font-lock-symbols )
+(setq haskell-font-lock-symbols t)
+
+(defun my-align-single-equals ()
+  "Align on a single equals sign (with a space either side)."
+  (interactive)
+  (align-regexp
+   (region-beginning) (region-end)
+   "\\(\\s-*\\) = " 1 0 nil))
+
+
+(define-key haskell-mode-map (kbd "C-a") 'my-align-single-equals )
+(define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
+(define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)
+
+
+
+(provide 'haskell-settings)
